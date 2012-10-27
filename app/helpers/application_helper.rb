9113731b0cbaa306aae7e0d2e1db8cab5f687076
nathan amick <github@nathanamick.com>
2012-10-27 17:17:15 +0000

    Fix states menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,16 @@ module ApplicationHelper
     glyph_path = "glyph/glyphicons_#{glyph}.png" 
     link_to(image_tag(glyph_path), link)
   end
+
+  def state_nav_li(text, path, icon, active)
+    icon_class = active ? "icon-#{icon} icon-white" : "icon-#{icon}"
+    li_class = active ? "active" : ""
+    i = content_tag('i', '', class: icon_class)
+    link = link_to(path) do
+      concat(i)
+      concat(" ")
+      concat(text)
+    end
+    content_tag('li', link, class: li_class)
+  end
 end
