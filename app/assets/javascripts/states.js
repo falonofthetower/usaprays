78bda9c32ab6ba775bdc948091d002469abbce79
nathan amick <github@nathanamick.com>
2012-11-05 03:55:09 +0000

    Comment out html comment

--- app/assets/javascripts/states.js
+++ app/assets/javascripts/states.js
@@ -1,4 +1,4 @@
-	<!--Below is the class function for the image map 'class=map'-->
+	// <!--Below is the class function for the image map 'class=map'-->
 	$(function() {
 		$('.map').maphilight({fade: false}); /*set true for fading hover effect*/
 		$('map > area').easyTooltip();  /*Tooltip function with area tag*/
