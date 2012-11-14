83cb05da6262b28218416eb7e7d3622578ef315d
nathan amick <github@nathanamick.com>
2012-11-14 15:08:13 +0000

    Fix finder on nil and empty quote

--- app/models/executive_selector.rb
+++ app/models/executive_selector.rb
@@ -7,7 +7,8 @@ class ExecutiveSelector
   end
 
   def self.for_day(state_code, date)
-    Refinery::Executives::Executive.where(state_code: nil).order("position") +
+    t = Refinery::Executives::Executive.arel_table
+    Refinery::Executives::Executive.where(t[:state_code].eq(nil).or(t[:state_code].eq(""))).order("position") +
     Refinery::Executives::Executive.where(state_code: state_code).order("position") 
   end
 
