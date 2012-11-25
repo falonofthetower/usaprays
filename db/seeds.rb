75ca55637224ed601178002e3a9f16b4ab6e3c78
nathan amick <github@nathanamick.com>
2012-11-25 14:08:08 -0600

    Add first user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,15 @@ Refinery::Justices::Engine.load_seed
 
 # Added by Refinery CMS Executives extension
 Refinery::Executives::Engine.load_seed
+
+admin = Refinery::User.new
+admin.username = 'admin'
+admin.email = 'admin@admin.com'
+admin.password = 'admin'
+admin.password_confirmation = 'admin'
+
+if admin.create_first
+  puts "Creating first user with username:  admin, password: admin.  Change this password!"
+else
+  puts "Failed to create first user"
+end
