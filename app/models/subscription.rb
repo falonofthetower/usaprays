80269afce3dbeb620532c8ac467a5e8ea913370f
Loren Harrell <Loren.Harrell@MemberHub.com>
2013-04-11 09:28:28 -0400

    Require both email AND name on the email signup form

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,7 +1,7 @@
 class Subscription
   include ActiveModel::Validations
 
-  validates_presence_of :email
+  validates_presence_of :email, :name
   validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
 
   attr_accessor :email, :name, :address, :city, :state, :zipcode, :state_code, :cycle
