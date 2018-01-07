7144f11da0b1d65bbd6cc765bfb95679280fd303
Peter Karth <falonofthetower@gmail.com>
2018-01-07 16:14:39 -0500

    corrected bucket name

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -2,7 +2,7 @@ require 'dragonfly'
 
 app = Dragonfly[:images]
 app.datastore = Dragonfly::DataStorage::S3DataStore.new({
-  :bucket_name        => ENV['BUCKETEER_AWS_BUCKET_NAME'],
+  :bucket_name        => ENV['BUCKETEER_BUCKET_NAME'],
   :access_key_id      => ENV['BUCKETEER_AWS_ACCESS_KEY_ID'],
   :secret_access_key  => ENV['BUCKETEER_AWS_SECRET_ACCESS_KEY']
 })
