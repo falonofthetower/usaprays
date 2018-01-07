2eb941be17648475d1960e6d1decadfa24645b39
Peter Karth <falonofthetower@gmail.com>
2018-01-07 15:52:03 -0500

    Setting up dragonfly

---
+++ config/initializers/dragonfly.rb
@@ -0,0 +1,8 @@
+require 'dragonfly/rails/images'
+
+app = Dragonfly[:images]
+app.datastore = Dragonfly::DataStorage::S3DataStore.new({
+  :bucket_name        => ENV['BUCKETEER_AWS_BUCKET_NAME'],
+  :access_key_id      => ENV['BUCKETEER_AWS_ACCESS_KEY_ID'],
+  :secret_access_key  => ENV['BUCKETEER_AWS_SECRET_ACCESS_KEY']
+})
